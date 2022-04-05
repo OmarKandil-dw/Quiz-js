c657f221dbc6f8a932f6f9f4affbee3e68cc07ea
omar_kandil <93253683+OmarKandil-dw@users.noreply.github.com>
2022-04-05 14:13:33 +0000

    first commit

---
+++ js.js
@@ -0,0 +1,100 @@
+const quizData = [
+    {
+        question: "Quel est le langage de programmation le plus utilisé en 2022 ?",
+        a: "Java",
+        b: "C",
+        c: "Node.js",
+        d: "javascript",
+        correct: "a",
+    },
+    {
+        question: "Quelle langue s'exécute dans un navigateur Web ??",
+        a: "Java",
+        b: "C++",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
+    },
+    {
+        question: "Que signifie HTML?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "En quelle année JavaScript a été lancé?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "2003",
+        correct: "b",
+    },
+
+
+];
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+       currentQuiz++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
